ee0457b222bea75d73c256c21385174e2448f8d5
Prince Sachdeva <scriptforprince@gmail.com>
2024-12-21 17:30:01 +0530

    Fetched kitty transactions ledger

--- public/assets/js/otpVerification.js
+++ public/assets/js/otpVerification.js
@@ -33,7 +33,7 @@ document.getElementById('otpVerificationForm').addEventListener('submit', async 
 }
 
   try {
-    const response = await fetch('https://bni-data-backend.onrender.com/api/auth/verify-otp', {
+    const response = await fetch('http://localhost:5000/api/auth/verify-otp', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, otp: otpCode }),
@@ -42,6 +42,8 @@ document.getElementById('otpVerificationForm').addEventListener('submit', async 
     const result = await response.json();
 
     if (result.success) {
+      localStorage.setItem('loggedInEmail', email);
+      localStorage.setItem('loginType', login_type);
       Swal.fire({
           icon: 'success',
           title: 'Login successful!',
